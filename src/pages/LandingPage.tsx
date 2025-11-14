1e9bdc7aaafd8c6ced7ecb8604f46174cc3640df
juanpab05 <juanpazcai07@gmail.com>
2025-11-13 19:36:16 -0500

    Added tailwind configuration

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -4,26 +4,26 @@ export const LandingPage: React.FC = () => {
   const navigate = useNavigate();
 
   return (
-    <div
-      className="relative w-full h-screen overflow-hidden pb-16"
-      role="banner"
-      aria-label="Landing page de Cinema Space"
-    >
-      {/* Background image */}
-      <img
-        src="/image-landing.jpg"
-        alt="Fondo con temática de películas y anime"
-        className="absolute top-0 left-0 w-full h-full object-cover brightness-75"
-      />
+    <div className="w-screen h-screen justify-center items-center bg-gray-500">
+        <div
+            className="relative w-full h-screen overflow-hidden pb-16 bg-gray-500"
+            role="banner"
+            aria-label="Landing page de Cinema Space"
+        >
 
-      {/* Overlay text layer */}
-      <div className="relative z-10 flex flex-col items-center justify-center h-full text-center text-white px-6">
-        <h1 className="text-3xl md:text-5xl font-bold mb-6">
-          Únete a <span className="text-red-500">Cinema Space</span> y descubre miles de películas
-        </h1>
-        <div className="flex gap-8">
+        {/* Overlay text layer */}
+        <div className="relative z-10 flex flex-col items-center justify-center h-full text-center text-white px-6">
+            <h1 className="text-3xl md:text-5xl font-bold mb-6">
+            Te ayudamos a 
+            <span className="text-blue-500"> conectar </span> 
+            con tus
+            <span className="text-blue-500"> amigos y familiares  </span> 
+            a distancia.
+            </h1>
+            <div className="flex gap-8">
+            </div>
         </div>
-      </div>
+        </div>
     </div>
   );
 };
